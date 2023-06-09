158a928cbe888d34d2cbcdf27b8823305128918d
DaniloFalcao <falcao_danilo@hotmial.com>
2023-06-09 15:04:44 -0300

    add

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: false,
   firebase: {
-   apiKey: "AIzaSyD0-fJViNe8ComHqnnddoTCrJqXMpo5V38",
-  authDomain: "crud-angular-74351.firebaseapp.com",
-  databaseURL: "https://crud-angular-74351-default-rtdb.firebaseio.com",
-  projectId: "crud-angular-74351",
-  storageBucket: "crud-angular-74351.appspot.com",
-  messagingSenderId: "711847779722",
-  appId: "1:711847779722:web:39ca0e7101f20374387c73"
+    apiKey: 'xxx',
+    authDomain: 'bezkoder-firebase.firebaseapp.com',
+    databaseURL: 'https://bezkoder-firebase.firebaseio.com',
+    projectId: 'bezkoder-firebase',
+    storageBucket: 'bezkoder-firebase.appspot.com',
+    messagingSenderId: 'xxx',
+    appId: 'xxx'
   }
 };
